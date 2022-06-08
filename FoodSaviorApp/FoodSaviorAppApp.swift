b1f7e210a275ff0c99025282da896010e284295f
Himani <himani.tawade@csu.fullerton.edu>
2022-06-08 11:12:30 -0700

    Initial Commit

---
+++ FoodSaviorApp/FoodSaviorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodSaviorAppApp.swift
+//  FoodSaviorApp
+//
+//  Created by csuftitan on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodSaviorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4151fad16ebece75a9c70d0be8651caf75f3f0ae
ManthanVasani <manthannvasani123@gmail.com>
2022-06-12 14:15:47 -0700

    Checkbox implemented in recipes

--- FoodSaviorApp/Recipes/SearchBasedOnIngredientsView.swift
+++ FoodSaviorApp/Recipes/SearchBasedOnIngredientsView.swift
@@ -7,14 +7,44 @@
 
 import SwiftUI
 
-struct SearchBasedOnIngredientsView: View {
-    var body: some View {
-        Text("Search based on ingrediants!")
-    }
+struct Task: Identifiable {
+    let id = UUID()
+    let name: String
+    var isCompleted: Bool
 }
 
-struct SearchBasedOnIngredientsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchBasedOnIngredientsView()
-    }
-}
+struct SearchBasedOnIngredientsView: View {
+    
+    @State private var tasks = [Task(name: "Potatoes", isCompleted: false), Task(name: "Onions", isCompleted: false), Task(name: "Tomatoes", isCompleted: false), Task(name: "Eggs", isCompleted: false), Task(name: "Milk", isCompleted: false), Task(name: "Corn", isCompleted: false), Task(name: "Spinach", isCompleted: false), Task(name: "Peas", isCompleted: false), Task(name: "Onions", isCompleted: false), Task(name: "Rice", isCompleted: false), Task(name: "Bread", isCompleted: false),  ]
+    
+    var body: some View {
+        VStack {
+                    List($tasks) { $task in
+                        TaskCellView(task: $task)
+                    }
+                    
+                    Text("Selected number if ingredients: \(tasks.filter { $0.isCompleted }.count)")
+                }
+            }
+        }
+
+        struct SearchBasedOnIngredientsView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+
+        struct TaskCellView: View {
+            
+            @Binding var task: Task
+            
+            var body: some View {
+                HStack {
+                    Image(systemName: task.isCompleted ? "checkmark.square": "square")
+                        .onTapGesture {
+                            task.isCompleted.toggle()
+                        }
+                    Text(task.name)
+                }
+            }
+        }

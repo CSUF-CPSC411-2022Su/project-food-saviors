15ef9ccc2280b8c877a68b07fc0638f0838231e5
ManthanVasani <manthannvasani123@gmail.com>
2022-06-17 04:34:54 -0700

    Update youtube web view

--- FoodSaviorApp/Recipes/RecipesView.swift
+++ FoodSaviorApp/Recipes/RecipesView.swift
@@ -36,13 +36,38 @@ struct RecipesView: View {
                     .navigationTitle("Find a recipe")
                 }
             }
+            
         }
         
-        VStack{
+        VStack {
+            
+           
+                GroupBox(label: Text("Ultimate Juicy Burger Recipe - Perfect Burgers Every Time 🍔")) {
+                    Text("he Perfect Juicy Burger Recipe! We partnered with Beef. It’s What’s for Dinner. to show you all of our secrets and tips for grilling the best burgers! I hope Chuck Knows Beef makes your time in the kitchen easier!")
+                    Text("By Natashas Kitchen")
+                }
+            GroupBox(label: Text("The Best Homemade Pizza You'll Ever Eat")) {
+                Text("Get the recipe for the best homemade pizza")
+                Text("By Tasty")
+            }
+            GroupBox(label: Text("How To Make The Best French Toast")) {
+                Text("This iconic dish is all about the details. While french toast might seem simple, looks can be deceiving. To get that perfectly crispy exterior and creamy, silky custard on the inside we spent weeks eating tons of butter, bread, and syrup to discover the best classic french toast recipe. The end result is indulgent, delicious, and most importantly, easy to make. What’s not to love?")
+                Text("By Tasty")
+            }
+            GroupBox(label: Text("Flourless Moist Chocolate Cake / Gluten Free / No Flour")) {
+                Text("Flourless Chocolate Cake (pan size 38 x 26 cm or 15 x 10 in 1 cup = 240ml; 1 tbsp = 15ml; 1 tsp = 5ml")
+                Text("By Thy Than")
+            }
+            GroupBox(label: Text("How to Make Egg Pasta (an in-depth guide)")) {
+                Text("How to Make Egg Pasta (an in-depth guide) 00 flour vs all-purpose, food processor vs the well method, rolling pin vs machine")
+                Text("By Helen Rennie")
+            }
+                
             
             
         }
         
+       
        
         
     }
